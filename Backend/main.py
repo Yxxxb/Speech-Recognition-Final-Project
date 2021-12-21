1d3392a4a95597bcc08b315f314a0913fc08421c
Yxxxb <2947934317@qq.com>
2021-12-21 13:22:12 +0800

    feat : 语音转文字

--- Backend/main.py
+++ Backend/main.py
@@ -1,37 +1,42 @@
 from head import create_app
-from flask import render_template,flash,redirect,url_for,request
+from flask import render_template, flash, redirect, url_for, request
 from webform import inputform
 import os
 
-# from voiceprint_predict import run_voiceprint
-# from emotion_predict import run_emotion
 
-def run_voiceprint(wave_path1,wave_path2):
+from voiceprint_predict import run_voiceprint
+from emotion_predict import run_emotion
+
+def run_voiceprint(wave_path1, wave_path2):
     print("run_voiceprint")
 
-def run_emotion(wave_path):
-    print("run_emotion",'wave_path is'+str(wave_path))
-    emotion='fear'
-    gender='male'
-    path_speech='images/speech.jpg'
-    path_emotion='images/emotion.jpg'
-    return emotion,gender,path_speech,path_emotion
+
+# def run_emotion(wave_path):
+#     print("run_emotion", 'wave_path is' + str(wave_path))
+#     emotion = 'fear'
+#     gender = 'male'
+#     path_speech = 'images/speech.jpg'
+#     path_emotion = 'images/emotion.jpg'
+#     return emotion, gender, path_speech, path_emotion
+
+
 # fear male images/speech.jpg images/emotion.jpg
 
 
 def return_img_stream(path):
     import base64
-    img_stream=''
-    with open(path,'rb') as img_f:
-        img_stream=img_f.read()
-        img_stream=base64.b64encode(img_stream).decode()
+    img_stream = ''
+    with open(path, 'rb') as img_f:
+        img_stream = img_f.read()
+        img_stream = base64.b64encode(img_stream).decode()
     print(img_stream)
     return img_stream
 
 
-app=create_app()
+app = create_app()
 
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     form = inputform()
     if form.validate_on_submit():
@@ -51,7 +56,7 @@ def index():
             for i in range(1, 99):
                 if i == 98:
                     flash("同名文件过多！！")
-                    form.file.data=None
+                    form.file.data = None
                     return render_template("index.html",
                                            form=form)
                 if not os.path.exists(src + '/' + str(oldhead) + "(" + str(i) + ")" + "." + back):
@@ -60,24 +65,25 @@ def index():
             road = src + '/' + str(newhead + '.' + back)
             form.file.data.save(road)
             flash("文件上传成功")
-            wave_path='test_data/'+str(newhead+'.'+back)
-            return redirect(url_for('result',wave_path=wave_path))
+            wave_path = 'test_data/' + str(newhead + '.' + back)
+            return redirect(url_for('result', wave_path=wave_path))
         else:
             road = src + '/' + str(form.file.data.filename)
             form.file.data.save(road)
             flash("文件上传成功")
-            wave_path='test_data/'+ str(form.file.data.filename)
-            return redirect(url_for('result',wave_path=wave_path))
+            wave_path = 'test_data/' + str(form.file.data.filename)
+            return redirect(url_for('result', wave_path=wave_path))
 
     return render_template("index.html",
                            form=form)
 
-@app.route("/result",methods=['GET'])
+
+@app.route("/result", methods=['GET'])
 def result():
-    wave_path=request.args.get("wave_path")
-    emotion,gender,path_speech,path_emotion=run_emotion(wave_path)
-    img_speech_stream=return_img_stream(path_speech)
-    img_emotion_stream=return_img_stream(path_emotion)
+    wave_path = request.args.get("wave_path")
+    emotion, gender, path_speech, path_emotion = run_emotion(wave_path)
+    img_speech_stream = return_img_stream(path_speech)
+    img_emotion_stream = return_img_stream(path_emotion)
     return render_template("result.html",
                            emotion=emotion,
                            gender=gender,
@@ -85,9 +91,5 @@ def result():
                            img_emotion_stream=img_emotion_stream)
 
 
-if __name__=='__main__':
-    app.run(port=5000,debug=True)
-
-
-
-
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
