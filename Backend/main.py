e6830b661638f53ed64a37279a57a5242e6d0165
lxy-146 <yiquyu@163.com>
2021-12-27 21:23:10 +0800

    加载页面

--- Backend/main.py
+++ Backend/main.py
@@ -3,42 +3,44 @@ from flask import render_template, flash, redirect, url_for, request
 from webform import inputform
 import os
 import time
+import urllib.parse
 
-from voiceprint_predict import run_voiceprint
-from emotion_predict import run_emotion
-from text_predict import run_text
-from volume_predict import run_DB
 
-# def run_text(wave_path):
-#     print("run_text")
-#     time = '259'
-#     text = '近几年不但我用书给女儿压岁也劝说朋不要给女儿压岁钱改送压岁书'
-#     score = 99.08357018079514
-#     return time, text, score
-#
-#
-# def run_voiceprint(wave_path1, wave_path2):
-#     print("run_voiceprint")
-#     is_one = True
-#     similarity = 0.9
-#     time.sleep(100)
-#     return is_one, similarity
-#
-#
-# def run_emotion(wave_path):
-#     print("run_emotion", 'wave_path is ' + str(wave_path))
-#     emotion = 'fear'
-#     gender = 'male'
-#     path_speech = 'images/emotion_test_speech.jpg'
-#     path_emotion = 'images/emotion_test_emotion.jpg'
-#     path_DB = 'images/emotion_test_DB.jpg'
-#     return emotion, gender, path_speech, path_emotion
-#
-# def run_DB(wave_path):
-#     DB=100
-#     path_DB = 'images/emotion_test_DB.jpg'
-#
-#     return DB,path_DB
+# from voiceprint_predict import run_voiceprint
+# from emotion_predict import run_emotion
+# from text_predict import run_text
+# from volume_predict import run_DB
+
+def run_text(wave_path):
+    print("run_text")
+    time = '259'
+    text = '近几年不但我用书给女儿压岁也劝说朋不要给女儿压岁钱改送压岁书'
+    score = 99.08357018079514
+    return time, text, score
+
+
+def run_voiceprint(wave_path1, wave_path2):
+    print("run_voiceprint")
+    is_one = True
+    similarity = 0.9
+    time.sleep(2)
+    return is_one, similarity
+
+
+def run_emotion(wave_path):
+    print("run_emotion", 'wave_path is ' + str(wave_path))
+    emotion = 'fear'
+    gender = 'male'
+    path_speech = 'images/yxb1(7)_speech.jpg'
+    path_emotion = 'images/yxb1(7)_emotion.jpg'
+    return emotion, gender, path_speech, path_emotion
+
+
+def run_DB(wave_path):
+    DB = 100
+    path_DB = 'images/yxb1(7)_DB.jpg'
+
+    return DB, path_DB
 
 
 def return_img_stream(path):
@@ -105,56 +107,62 @@ def index():
         road2 = src + '/' + new_name2
         form.file2.data.save(road2)
         wave_path2 = 'test_data/' + new_name2
-        return redirect(url_for('result',
+        return redirect(url_for('loading',
                                 wave_path1=wave_path1,
                                 wave_path2=wave_path2))
 
     return render_template("index.html",
                            form=form)
 
+
+@app.route('/loading',methods=['GET'])
+def loading():
+    some_data = "Here's some example data"
+    some_data = urllib.parse.quote(some_data)
+    return render_template('loading.html', my_data = some_data)
+
 @app.route("/result", methods=['GET'])
 def result():
-    is_one=None
-    if is_one:
-        return render_template("loading.html")
-    else:
-        wave_path1 = request.args.get("wave_path1")
-        wave_path2 = request.args.get("wave_path2")
-        emotion1, gender1, path_speech1, path_emotion1 = run_emotion(wave_path1)
-        emotion2, gender2, path_speech2, path_emotion2 = run_emotion(wave_path2)
-        DB1,path_DB1=run_DB(wave_path1)
-        DB2,path_DB2=run_DB(wave_path2)
-        img_speech_stream1 = return_img_stream(path_speech1)
-        img_speech_stream2 = return_img_stream(path_speech2)
-        img_emotion_stream1 = return_img_stream(path_emotion1)
-        img_emotion_stream2 = return_img_stream(path_emotion2)
-        img_DB_stream1 = return_img_stream(path_DB1)
-        img_DB_stream2 = return_img_stream(path_DB2)
-        time1, text1, score1 = run_text(wave_path1)
-        time2, text2, score2 = run_text(wave_path2)
-        is_one, similarity = run_voiceprint(wave_path1, wave_path2)
-        return render_template("result.html",
-                               is_one=is_one,
-                               similarity=similarity,
-                               emotion1=emotion1,
-                               gender1=gender1,
-                               DB1=DB1,
-                               img_speech_stream1=img_speech_stream1,
-                               img_emotion_stream1=img_emotion_stream1,
-                               img_DB_stream1=img_DB_stream1,
-                               emotion2=emotion2,
-                               gender2=gender2,
-                               DB2=DB2,
-                               img_speech_stream2=img_speech_stream2,
-                               img_emotion_stream2=img_emotion_stream2,
-                               img_DB_stream2=img_DB_stream2,
-                               time1=time1,
-                               text1=text1,
-                               score1=score1,
-                               time2=time2,
-                               text2=text2,
-                               score2=score2)
+    data = "No data was passed"
+    if request.args.to_dict(flat=False)['data'][0]:
+        pass
+    wave_path1 = request.args.get("wave_path1")
+    wave_path2 = request.args.get("wave_path2")
+    emotion1, gender1, path_speech1, path_emotion1 = run_emotion(wave_path1)
+    emotion2, gender2, path_speech2, path_emotion2 = run_emotion(wave_path2)
+    DB1, path_DB1 = run_DB(wave_path1)
+    DB2, path_DB2 = run_DB(wave_path2)
+    img_speech_stream1 = return_img_stream(path_speech1)
+    img_speech_stream2 = return_img_stream(path_speech2)
+    img_emotion_stream1 = return_img_stream(path_emotion1)
+    img_emotion_stream2 = return_img_stream(path_emotion2)
+    img_DB_stream1 = return_img_stream(path_DB1)
+    img_DB_stream2 = return_img_stream(path_DB2)
+    time1, text1, score1 = run_text(wave_path1)
+    time2, text2, score2 = run_text(wave_path2)
+    is_one, similarity = run_voiceprint(wave_path1, wave_path2)
+    return render_template("result.html",
+                           is_one=is_one,
+                           similarity=similarity,
+                           emotion1=emotion1,
+                           gender1=gender1,
+                           DB1=DB1,
+                           img_speech_stream1=img_speech_stream1,
+                           img_emotion_stream1=img_emotion_stream1,
+                           img_DB_stream1=img_DB_stream1,
+                           emotion2=emotion2,
+                           gender2=gender2,
+                           DB2=DB2,
+                           img_speech_stream2=img_speech_stream2,
+                           img_emotion_stream2=img_emotion_stream2,
+                           img_DB_stream2=img_DB_stream2,
+                           time1=time1,
+                           text1=text1,
+                           score1=score1,
+                           time2=time2,
+                           text2=text2,
+                           score2=score2)
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True,threaded=True)
